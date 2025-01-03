6c54de243a662d5c3c53cb537ee0a00e43391fe5
unknown <shashibhanmaurya729@gmail.com>
2025-01-03 02:50:06 -0800

    start

---
+++ 3_JAV_55.js
@@ -0,0 +1,27 @@
+//Variables:
+console.log(5+5)
+
+// Variable is a the location to  store data.
+// Which is easly accessible in futureuse.
+
+var x=34
+console.log(x) 
+
+// Const
+// Let
+
+// --------------Rules of Variables.
+// 1.Varialbe can not start with number.
+// 2.Varialbe can not start with any symbol ,excluded(_)
+// let _34=22;
+// 3.Variable can not include whitespaces.
+// 4.Variabls in javascript are case-senstive.
+
+//---------------------------------------------------------------------------
+// Data Types in javascript.
+
+// 1.Primitive data types.
+// Numbers, string,undefined,Null,symbol,object,boolean,object
+
+// 2.Nonprimitive data types.
+// 
