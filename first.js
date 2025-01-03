00918def125eb27cbe885c069c7e006a4d045649
unknown <shashibhanmaurya729@gmail.com>
2025-01-03 01:49:40 -0800

    start

---
+++ first.js
@@ -0,0 +1,8 @@
+ alert()
+ var num1=345
+ var num2=40
+ document.write(`Result of Addition:${num1+num2}`)
+ document.writeln(`Result of substraction:${num1-num2}`)
+ document.write(`Result of Multiplicatio:${num1*num2}`)
+ document.write(`Result of Division:${num1/num2}`)
+ document.write(`Result of Modulas:${num1%num2}`)
---
+++ second.js
@@ -0,0 +1,13 @@
+console.log("SHASHI BHAN MAURYA")
+
+var address="Lucknow"
+let number="12344"
+const a=112
+numberid=234
+
+address="Gonda"
+number ="1256"
+numberid=345
+
+console.table([address,number,a,numberid])
+
