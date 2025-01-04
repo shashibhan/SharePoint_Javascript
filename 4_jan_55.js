d8164a5e8f89f49b677b6cb429f6cbcc92ad127b
unknown <shashibhanmaurya729@gmail.com>
2025-01-04 02:38:09 -0800

    js

---
+++ 4_jan_55.js
@@ -0,0 +1,14 @@
+let name1="IQ INDIA"
+document.write(`<center><h1>JAVASCRIPT</h1></center>`)
+document.write(`<center><h1>${name1}</h1></center>`)
+document.write(`<hr>`)
+
+let num1=22
+let num2=11
+let res=num1+num2
+document.write(`<h1> <span style="color:green ">${num1} </span> + ${num2} = ${res}</h1>`)
+
+let n1 =Number(Input("Enter Your First Number : "))
+let n2 =Number(input("Enter Your Second Number : "))
+let total=n1+n2
+document.write(`Result= ${total}`)
